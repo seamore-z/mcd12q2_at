198b1255a580d2272c6572e46552ce4d7a0dc5ea
seamore-z <zhu.seamore@gmail.com>
2024-11-09 09:16:34 -0500

    Calling new download scripts

--- run_nbara2.sh
+++ run_nbara2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo Submitting $1
-R --vanilla < /projectnb/modislc/users/seamorez/mcd12q2_at/get_MCD43A2.R $1
-
+#R --vanilla < /projectnb/modislc/users/seamorez/mcd12q2_at/get_MCD43A2.R $1
+R --vanilla < /projectnb/modislc/users/seamorez/mcd12q2_at/get_MCD43A2_all.R $1
 
--- run_nbara4.sh
+++ run_nbara4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo Submitting $1
-R --vanilla < /projectnb/modislc/users/seamorez/mcd12q2_at/get_MCD43A4.R $1
-
+#R --vanilla < /projectnb/modislc/users/seamorez/mcd12q2_at/get_MCD43A4.R $1
+R --vanilla < /projectnb/modislc/users/seamorez/mcd12q2_at/get_MCD43A4_all.R $1
 
